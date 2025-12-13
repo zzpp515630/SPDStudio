156c24563592dba27e92dddd9046647d701cbd13
makeit <makeit@oonet.icu>
2025-12-13 14:52:45 +0800

    chore: bump version to 1.1.0

--- src/utils/version.py
+++ src/utils/version.py
@@ -2,8 +2,8 @@
 版本信息和 GitHub 常量
 """
 
-__version__ = "1.0.0"
-__version_info__ = (1, 0, 0)
+__version__ = "1.1.0"
+__version_info__ = (1, 1, 0)
 
 GITHUB_REPO = "lvusyy/SPDStudio"
 GITHUB_API_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
